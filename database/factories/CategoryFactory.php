3d7a710441e76a8e294d706c01ec03a739c3d04a
deen8122 <deen812@mail.ru>
2022-07-20 21:53:54 +0500

    add migrations, seeders

---
+++ database/factories/CategoryFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Category>
+ */
+class CategoryFactory extends Factory
+{
+    private int $interation = 1;
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name' => "Категория ".$this->getIteration(),
+        ];
+    }
+
+    private function getIteration(){
+        return $this->interation++;
+    }
+}
---
+++ database/factories/ProductCategoryFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Product;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\ProductCategory>
+ */
+class ProductCategoryFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            "product_id"=>rand(1,50),
+            "category_id"=>rand(1,10),
+        ];
+    }
+}
