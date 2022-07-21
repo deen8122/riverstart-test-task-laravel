a85575527660a92b3bbcc6aa8d650200a00dfc34
deen8122 <deen812@mail.ru>
2022-07-21 17:49:11 +0500

    add docker

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    $users  = \App\Models\User::all()->toArray();
+    print_r($users);
     return view('welcome');
 });
